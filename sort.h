bab1643a8f71c1fc133946544131123ca11855fe
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-09-27 16:20:25 +0100

    chore: Update sort.h file with improved comments and formatting

--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /*Comparison direction macros for bitonic sort*/
 #define UP 0
@@ -30,21 +31,20 @@ typedef enum bool
 */
 typedef struct listint_s
 {
-	const int n;
+const int n;
 
-	struct listint_s *prev;
-	struct listint_s *next;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
-/*Helper for swapping*/
-void swap_ints(int *a, int *b);
+/* Print Functions */
 
-/*Printing helper functions */
 void print_array(const int *array, size_t size);
 
 void print_list(const listint_t *list);
 
-/*Regular functions prototypes*/
+/* Sorting Algorithms Functions */
+
 void bubble_sort(int *array, size_t size);
 
 void insertion_sort_list(listint_t **list);
@@ -52,6 +52,20 @@ void selection_sort(int *array, size_t size);
 
 void quick_sort(int *array, size_t size);
 
+/*Advanced Tasks*/
+void shell_sort(int *array, size_t size);
 
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 
-#endif
+void merge_sort(int *array, size_t size);
+
+void heap_sort(int *array, size_t size);
+
+void radix_sort(int *array, size_t size);
+
+void bitonic_sort(int *array, size_t size);
+
+void quick_sort_hoare(int *array, size_t size);
+
+#endif /* SORT_H */
