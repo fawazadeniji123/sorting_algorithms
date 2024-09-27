f4300c5d4e343dbd8adbb7521efaf3ad92e7e315
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-09-27 15:55:12 +0100

    chore: Update sort.h file with improved comments and formatting

--- sort.h
+++ sort.h
@@ -8,26 +8,32 @@
 #define UP 0
 #define DOWN 1
 
-/*enum bool - Enumeration of boolean values*/
+/**
+* enum bool - Boolean data type
+*
+* @false: 0
+* @true: 1
+*/
 typedef enum bool
 {
-    false = 0,
-    true
+	false = 0,
+	true
 } bool;
 
 
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
+*/
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /*Helper for swapping*/
@@ -35,12 +41,15 @@ void swap_ints(int *a, int *b);
 
 /*Printing helper functions */
 void print_array(const int *array, size_t size);
+
 void print_list(const listint_t *list);
 
 /*Regular functions prototypes*/
 void bubble_sort(int *array, size_t size);
+
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+
 void quick_sort(int *array, size_t size);
 
 
